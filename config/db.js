d5a92916b479b509a8a627c0d9337688a1573e0f
arjayhapa00@gmail.com <arjayhapa00@gmail.com>
2025-10-23 22:33:10 +0800

    Delete config directory

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB() {
-const uri = process.env.MONGO_URI;
-if (!uri) throw new Error('MONGO_URI missing');
-await mongoose.connect(uri);
-console.log('膆 Mongo connected');
-}
-module.exports = { connectDB };

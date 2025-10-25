a8ad27d54f014ca4b9e1d05545846d915932fe01
arjayhapa00@gmail.com <arjayhapa00@gmail.com>
2025-10-25 14:49:06 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error('MONGODB_URI missing');
+const uri = process.env.MONGO_URI;
+if (!uri) throw new Error('MONGO_URI missing');
 await mongoose.connect(uri);
-console.log('膆 Mongodb connected');
+console.log('膆 Mongo connected');
 }
 module.exports = { connectDB };
